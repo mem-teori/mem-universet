04cddbf66b89b241419f611b996716a7e2285a74
Maria Edlenborg <m.e.m.teori@gmail.com>
2026-08-20 23:52:50 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig({
-  plugins:[react(),{name:"mem-pages-paths",transform(code,id){return id.endsWith("style.css")?code.replace("url('/yggdrasil-gate.png')","url('./yggdrasil-gate.png')"):null}}],
-  base:"./"
-});
